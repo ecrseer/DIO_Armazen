f7fa4fa68fc0f8c3ef7df86fc63af72ad7c6eb17
Gabriel Justino <60169516+ecrseer@users.noreply.github.com>
2020-12-25 21:18:03 -0300

    Initial commit

---
+++ Models/Utilitario.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DIO_Armazen.Models
+{
+    public class Utilitario
+    {
+        public int id { get; set; }
+        public string descricao { get; set; }
+    }
+}
