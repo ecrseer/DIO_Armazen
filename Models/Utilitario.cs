47db7f32be8a63cfabaa98e4b66f4ebf84bd4772
Gabriel Justino <60169516+ecrseer@users.noreply.github.com>
2020-12-27 19:01:17 -0300

    annotation: display descricao better

--- Models/Utilitario.cs
+++ Models/Utilitario.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,7 @@ namespace DIO_Armazen.Models
     public class Utilitario
     {
         public int id { get; set; }
+        [Display(Name ="Descrição")]
         public string descricao { get; set; }
         public List<Produto> produtos { get; set; }
     }
